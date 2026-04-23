9f290bb7986e24880f39b333ae2d0fed3e67fb38
Stanislav Aleksandrov <stasi06a@gmail.com>
2026-04-23 23:53:58 +0200

    change startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql18
-gunicorn --bind 0.0.0.0:8000 app:app
+set -e
+
+# Install ODBC Driver 18
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+curl -fsSL https://packages.microsoft.com/config/debian/12/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update -qq
+ACCEPT_EULA=Y apt-get install -y -q msodbcsql18
+
+# Start gunicorn
+exec gunicorn --bind 0.0.0.0:8000 app:app
