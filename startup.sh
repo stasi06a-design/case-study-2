b3fa5ddb5ae7f93bc2d2364ffbaef5221fcb5104
Stanislav Aleksandrov <stasi06a@gmail.com>
2026-04-23 23:33:19 +0200

    startup.sh added

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
+gunicorn --bind 0.0.0.0:8000 app:app
